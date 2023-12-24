d6f07fc58cc5a9a280201443f634dd62e3929aa3
jarviswynn123 <jmwynn11998@gmail.com>
2023-12-24 12:14:43 -0500

    Code-Review-6-completed

--- use lego;.sql
+++ use lego;.sql
@@ -17,26 +17,28 @@ SELECT id, name FROM part_categories AS p WHERE name LIKE "%Bricks%";
 --The final statement gets the name column from the sets table where the theme_id is the same as the id's from the original query
 SELECT name FROM sets WHERE theme_id IN (SELECT id from themes WHERE name LIKE "%Pirates%" or name LIKE "%Star Wars%");
 
-
+--This statement gets all columns from the inventory parts table where the inventory_id is the same as the id in the inventories table with a version greater than 1.
 SELECT * FROM inventory_parts WHERE inventory_id IN (SELECT id FROM inventories WHERE version > 1)
 
-
+--This statement gets year and name from the sets table that includes the substring of batman. The name column is manipulated. All items in name are capitolized and concatenated with "!!!"
 SELECT year, CONCAT(UPPER(name), "!!!") FROM sets as s WHERE name LIKE "%Batman%"
 
-SELECT quantity FROM inventory_parts WHERE quantity > 1
-
+--Each value in the quantity column within the inventory_parts table is doubled where the value is originally greater than 1. The display is limited by 20
 UPDATE inventory_parts
 SET inventory_parts.quantity = inventory_parts.quantity*2
 WHERE inventory_parts.quantity > 1
-
 SELECT * FROM inventory_parts WHERE quantity > 1 ORDER BY quantity LIMIT 20;
 
+--sum of all the parts for all the sets that came out since the year 2000
 SELECT *, SUM(num_parts) AS all_parts FROM sets WHERE year >= 2000
 
+--average number of parts for sets that came out since the year 2000
 SELECT *, AVG(num_parts) AS avg_num_parts FROM sets WHERE year >= 2000
 
+--average number of parts for each theme_id in sets
 SELECT theme_id, AVG(num_parts) AS avg_num_parts From sets GROUP BY theme_id
 
+--join the parts and part_categories tables. Filter for values where the part_categories id matches the parts part_cat_id, and where the name of the part contains the word 'Werewolf'
 SELECT *
 FROM parts
 JOIN part_categories
